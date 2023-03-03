6df9cf78c4f53a5accbe9c4a82437907747fcfaf
Larry Z <13360148+FlickerSoul@users.noreply.github.com>
2023-03-03 01:12:16 -0800

    fix: add og icon meta

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { useHead } from '@vueuse/head'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import { isDark } from './logics'
 
 useHead({
   meta: [
     { property: 'og:title', content: 'Universe Observer' },
-    { property: 'og:icon', content: '' }, // TODO setup icons
+    { property: 'og:icon', content: isDark ? '/logo-white-lower' : '/logo-black-lower' },
+    { property: 'og:type', content: 'website' },
     { name: 'description', content: 'Larry Z\'s blog' },
   ],
 })
