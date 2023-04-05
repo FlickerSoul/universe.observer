74c09c24a98ee253b3d5b9120d0f6d53dcbb8ed6
Larry Z <13360148+FlickerSoul@users.noreply.github.com>
2023-04-05 02:28:01 -0700

    fix: copying button

--- src/components/CodeCopyButton.vue
+++ src/components/CodeCopyButton.vue
@@ -5,7 +5,7 @@ import { useClipboard } from '@vueuse/core'
 const parent = ref<HTMLDivElement>()
 function copyParentText() {
   const codeNode: HTMLPreElement | null | undefined
-    = parent.value?.parentElement?.parentElement?.querySelector('pre.shiki')
+    = parent.value?.parentElement?.parentElement?.parentElement?.querySelector('pre.shiki')
   if (codeNode?.textContent) {
     const { isSupported, copy } = useClipboard()
     copy(codeNode.textContent)
