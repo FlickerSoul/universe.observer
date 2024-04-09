ff85e1d105ae4ff13b090d8f33533c0d9599f874
Heyuan Zeng <hi@universe.observer>
2024-04-09 13:49:52 +0100

    fix: warnings of bad head usage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
 import { useRoute } from 'vue-router'
-import { onMounted, watch } from 'vue'
+import { onMounted } from 'vue'
 import { SpeedInsights } from '@vercel/speed-insights/vue'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
@@ -30,28 +30,22 @@ useHead({
   title: TITLE,
   meta: [
     { property: 'og:title', content: TITLE },
-    { property: 'og:icon', content: isDark ? '/logo-white-lower' : '/logo-black-lower' },
     { property: 'og:type', content: 'website' },
     { name: 'description', content: DESCRIPTION },
     { property: 'og:description', content: DESCRIPTION },
-    { property: 'og:url', content: `%site.url${route.fullPath}` },
   ],
 })
 
-watch(isDark, (val) => {
-  useHead({
-    meta: [
-      { property: 'og:icon', content: val ? '/logo-white-lower' : '/logo-black-lower' },
-    ],
-  })
+useHead({
+  meta: [
+    { property: 'og:icon', content: () => isDark.value ? '/logo-white-lower' : '/logo-black-lower' },
+  ],
 })
 
-watch(() => route.fullPath, (val) => {
-  useHead({
-    meta: [
-      { property: 'og:url', content: `%site.url${val}` },
-    ],
-  })
+useHead({
+  meta: [
+    { property: 'og:url', content: () => `%site.url${route.fullPath}` },
+  ],
 })
 </script>
 
