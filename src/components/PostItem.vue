806340a1c808b699e4b7764f70ff68bd04bca512
Heyuan Zeng <hi@universe.observer>
2024-04-20 15:41:50 +0100

    fix: handle inner link properly

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import LangIndicator from './LangIndicator.vue'
 import PostDate from './PostDate.vue'
 import type { IListedPostData } from './types'
 import PostAbstract from './PostAbstract.vue'
 
 const { post } = defineProps<{ post: IListedPostData }>()
+const router = useRouter()
+
+function handleClick(e: MouseEvent) {
+  const target = e.target as HTMLElement
+  if (target.tagName === 'A' && target.closest('a'))
+    return
+
+  router.push({ path: post.path })
+}
 </script>
 
 <template>
   <li>
-    <div class="post-item font-mono no-underline ">
+    <div class="post-item font-mono no-underline">
       <div class="align-middle md:ml--10.5 mr-0.6em inline-flex gap-2 flex-row-reverse md:w-25px">
         <router-link
           v-for="lang in (post.langs || [post.lang]).sort()"
@@ -20,9 +30,10 @@ const { post } = defineProps<{ post: IListedPostData }>()
           <LangIndicator :lang="lang" />
         </router-link>
       </div>
-      <router-link
-        :to="post.path"
-        class="post-body border-none hover:border-none opacity-50 hover:opacity-75 ml-0.4em"
+      <div
+        class="post-body inline cursor-pointer opacity-50 hover:opacity-100 ml-0.4em
+        transition-opacity transition-duration-500"
+        @click="handleClick"
       >
         <div class="md:inline-block block align-middle w-100%">
           <div class="flex md:flex-row flex-col md:place-items-center">
@@ -37,7 +48,7 @@ const { post } = defineProps<{ post: IListedPostData }>()
         </div>
         <PostDate v-bind="post" class="post-dates w-100%" />
         <PostAbstract v-if="post.description" :abstract="post.description" class="post-abstract w-100%" />
-      </router-link>
+      </div>
     </div>
   </li>
 </template>
